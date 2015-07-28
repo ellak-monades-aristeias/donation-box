0759248524c3365b52413567010e213a4b886836
dkoukoul <dimitris.koukoulakis@gmail.com>
2015-07-28 22:51:32 +0000

    -a

---
+++ copyfiles.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cp wordpress/wp-content/themes/influence/header.php /var/www/wp-content/themes/influence/header.php
+cp wordpress/wp-content/themes/influence/style.css /var/www/wp-content/themes/influence/style.css
