955b5562e4186a29937f76e14e895909434e3a27
dkoukoul <dimitris.koukoulakis@gmail.com>
2015-10-01 21:46:12 +0300

    Removed copyfiles.sh

--- copyfiles.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cp wordpress/wp-content/themes/influence/header.php /var/www/wp-content/themes/influence/header.php
-cp wordpress/wp-content/themes/influence/style.css /var/www/wp-content/themes/influence/style.css
